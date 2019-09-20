2ed38265ca7e5b11de4408315b1926986a756511
Sun Zhenhao <Zhenhaos@uci.edu>
2019-09-20 21:33:24 +0800

    Initial Robot Project

---
+++ src/containers/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+
+
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            robots:[],
+            searchfield:""
+        }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>response.json())
+        .then(users=> this.setState({robots:users}));
+    }
+
+    onSearchChange = (event)=>{
+        this.setState({searchfield:event.target.value}) 
+        }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robot=>{
+        return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())});
+        return(
+            <div className ='tc'>
+                <h1 className = 'f1'>RoboFriends</h1>
+                <SearchBox searchChange = {this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        );
+    }
+}
+
+export default App;
